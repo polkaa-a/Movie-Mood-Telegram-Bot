f2ebf3c7614a23de0f901a7abaed01a98fae61f9
Ekaterina <ekaterina.2001@live.com>
2022-11-20 17:10:00 +0300

    feat: add QuestionsProvider that provides string constants with questions for the bot

---
+++ src/main/java/tgbot/moviemoodbot/service/provider/QuestionsProvider.java
@@ -0,0 +1,14 @@
+package tgbot.moviemoodbot.service.provider;
+
+public class QuestionsProvider {
+    public static final String ASK_RATING = "Is the rating of the film important to you?";
+    public static final String ASK_GENRE = "What genre do you prefer?";
+    public static final String ASK_COUNTRY = "Is the country of the film important to you?";
+    public static final String ASK_GENRE_LOOP = "Would you like to choose one more genre?";
+    public static final String ASK_YEAR = "Is the year of premiere of the film important to you?";
+
+    public static final String CHOOSE_RATING = "Select the lower threshold of the rating value or click \"Any\"";
+    public static final String CHOOSE_COUNTRY = "Select the country or click \"Any\"";
+    public static final String CHOOSE_CAST = "Select the cast of the film";
+    public static final String CHOOSE_YEAR = "Select the higher threshold of the year of premiere or click \"Enter year\"";
+}
